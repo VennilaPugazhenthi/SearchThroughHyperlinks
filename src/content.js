177b98743d92726805262e2b7cccb121892a172a
Vennila Pugazhenthi <vennilapugazhenthi@gmail.com>
2020-06-01 11:33:35 -0400

    Lists all the Hyperlinks in a web page

--- src/content.js
+++ src/content.js
@@ -1,3 +1,16 @@
-chrome.runtime.onMessage.addListener(function (request) {
-    alert(request)
+chrome.runtime.onMessage.addListener(function (request,sender, sendResponse) {
+    const re = new RegExp(request,'gi')
+    const matches = document.documentElement.innerText.match(re)
+    // sendResponse({count: matches.length})
+
+    var x= document.querySelectorAll("a");
+    var myarray = []
+    for (var i=0;i<x.length; i++){
+        var nametext=x[i].textContent;
+        var cleantext= nametext.replace(/\s+/g,' ').trim();
+        var cleanlink= x[i].href;
+        // myarray.push([cleantext,cleanlink]);
+        myarray.push(cleanlink);
+    };
+    sendResponse({count: matches.length,list:myarray,len:x.length})
 })
--- src/popup.js
+++ src/popup.js
@@ -3,6 +3,26 @@ function clicked() {
     var val=document.getElementById("findInput").value;
     chrome.tabs.query({currentWindow:true, active:true},
         function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id,val)
+            chrome.tabs.sendMessage(tabs[0].id,val,setCount)
         })
+}
+
+function setCount(res) {
+    //Outputs number of appearance
+    const div =document.createElement('div')
+    div.textContent=res.count.toString()+' appearance'
+
+    document.body.appendChild(div)
+
+    //Outputs all the hyperlinks in the web page
+    var totalLinks=res.len;
+    document.getElementById("links").innerHTML="";
+    for (var i=0;i<totalLinks;i++){
+        var textlink=res.list[i].toString();
+        if(textlink.indexOf("http://")==0||textlink.indexOf("https://")==0){
+            document.getElementById("links").innerHTML+=res.list[i].toString()+"<br/>";
+        }
+
+    }
+
 }
