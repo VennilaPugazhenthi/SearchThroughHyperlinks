c72d20f7df5596888d85bad85f1458ebd69d3aaf
Vennila Pugazhenthi <vennilapugazhenthi@gmail.com>
2020-06-02 14:20:55 -0400

    Lists all the Hyperlinks in a web page as checkboxes

--- src/content.js
+++ src/content.js
@@ -1,8 +1,10 @@
 chrome.runtime.onMessage.addListener(function (request,sender, sendResponse) {
+    //Finds the matches of the searched word
     const re = new RegExp(request,'gi')
     const matches = document.documentElement.innerText.match(re)
     // sendResponse({count: matches.length})
 
+    //Extracts all the usefully hyperlinks and pushes it into an array
     var x= document.querySelectorAll("a");
     var myarray = []
     for (var i=0;i<x.length; i++){
--- src/popup.js
+++ src/popup.js
@@ -16,11 +16,26 @@ function setCount(res) {
 
     //Outputs all the hyperlinks in the web page
     var totalLinks=res.len;
-    document.getElementById("links").innerHTML="";
+    // document.getElementById("links").innerHTML="";
     for (var i=0;i<totalLinks;i++){
         var textlink=res.list[i].toString();
         if(textlink.indexOf("http://")==0||textlink.indexOf("https://")==0){
-            document.getElementById("links").innerHTML+=res.list[i].toString()+"<br/>";
+            // document.getElementById("links").innerHTML+=res.list[i].toString()+"<br/>";
+
+
+            var check=document.createElement("INPUT");
+            check.setAttribute("type","checkbox");
+            check.id=res.list[i].toString();
+
+            var label=document.createElement("label");
+            label.setAttribute('for',res.list[i].toString());
+            label.appendChild(document.createTextNode(res.list[i].toString()));
+
+            container.appendChild(check);
+            container.appendChild(label);
+            linebreak=document.createElement("br");
+            container.appendChild(linebreak);
+
         }
 
     }
