5bea4fa757769d897bd2188461c49b96eec268a5
Vennila Pugazhenthi <vennilapugazhenthi@gmail.com>
2022-12-20 21:00:10 -0700

    Reformat code

--- src/content.js
+++ src/content.js
@@ -1,29 +1,27 @@
-chrome.runtime.onMessage.addListener(function (request,sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     //Finds the matches of the searched word
-    const re = new RegExp(request,'gi')
+    const re = new RegExp(request, 'gi')
     const matches = document.documentElement.innerText.match(re)
 
     // Error Handling for appearance of searched word in current web page
     var appear_current;
-    try{
-        appear_current=matches.length;
-        console.log("The correct number of matches is:",appear_current);
-    }catch (e) {
-        if(e instanceof TypeError){
-            appear_current=0;
-            console.log("The correct number of matches is:",appear_current);
-        }else{
-            appear_current=0;
+    try {
+        appear_current = matches.length;
+    } catch (e) {
+        if (e instanceof TypeError) {
+            appear_current = 0;
+        } else {
+            appear_current = 0;
         }
     }
 
     // Highlights the matched words that are just text
-    var parents=[];
-    var children=[];
+    var parents = [];
+    var children = [];
 
     //Find all text nodes, and add their parents to a list
-    recursiveFindTextNodes(0,document.body,function (parent, node) {
-        if(node.data.match(re)){
+    recursiveFindTextNodes(0, document.body, function (parent, node) {
+        if (node.data.match(re)) {
             parents.push(parent);
             children.push(node);
         }
@@ -31,30 +29,28 @@ chrome.runtime.onMessage.addListener(function (request,sender, sendResponse) {
 
     //The list only contains elements with text as their children
     //Loop through the list and replace any matches
-    var count_color=0;
-    for(let i=0; i< parents.length; i++){
+    var count_color = 0;
+    for (let i = 0; i < parents.length; i++) {
         var parent = parents[i];
         var node = children[i];
         var div = document.createElement("span");
-        parent.replaceChild(div,node);
+        parent.replaceChild(div, node);
         div.innerHTML = node.data.replace(re, function (match) {
-            count_color=count_color+1;
-            return "<mark style='background-color:#FFF34D; border-radius: 5px;'>"+match+"</mark>";
+            count_color = count_color + 1;
+            return "<mark style='background-color:#FFF34D; border-radius: 5px;'>" + match + "</mark>";
         });
     }
-    //Logs number of highlights
-    console.log("Number of highlights:",count_color.toString());
 
     //Extracts all the usefully hyperlinks and pushes it into an array
-    var extractedLinks= document.querySelectorAll("a");
+    var extractedLinks = document.querySelectorAll("a");
     let hyperlinkMap = new Map();
-    for (var i=0;i<extractedLinks.length; i++){
-        var href_validility=extractedLinks[i].href.toString();
-        if(href_validility.indexOf("http://")==0||href_validility.indexOf("https://")==0){
-            var cleanlink= extractedLinks[i].href;
-            if(!hyperlinkMap.has(cleanlink)){
-                var nametext=extractedLinks[i].textContent;
-                var cleantext= nametext.replace(/\s+/g,' ').trim();
+    for (var i = 0; i < extractedLinks.length; i++) {
+        var href_validility = extractedLinks[i].href.toString();
+        if (href_validility.indexOf("http://") == 0 || href_validility.indexOf("https://") == 0) {
+            var cleanlink = extractedLinks[i].href;
+            if (!hyperlinkMap.has(cleanlink)) {
+                var nametext = extractedLinks[i].textContent;
+                var cleantext = nametext.replace(/\s+/g, ' ').trim();
                 hyperlinkMap.set(cleanlink, cleantext);
             }
         }
@@ -62,87 +58,71 @@ chrome.runtime.onMessage.addListener(function (request,sender, sendResponse) {
 
     getCountFromLinks(hyperlinkMap, re);
 
-    sendResponse({count: appear_current,list:hyperlinkMap.keys(),len:hyperlinkMap.size})
+    sendResponse({count: appear_current, list: hyperlinkMap.keys(), len: hyperlinkMap.size})
 
 })
 
-function getCountFromLinks(hyperlinkMap, re){
-    console.log("Inside getCountFromLink()");
+function getCountFromLinks(hyperlinkMap, re) {
     var max = hyperlinkMap.size;
-    for(let [link, linkText] of hyperlinkMap){
+    for (let [link, linkText] of hyperlinkMap) {
         var xhr = new XMLHttpRequest();
-        xhr.open("GET",link,true);
-
-        xhr.onreadystatechange = function(){
-            if(this.readyState == 4 && this.status == 200){
-                var response= this.responseText;
+        xhr.open("GET", link, true);
+        xhr.timeout = 1500;
+        xhr.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                var response = this.responseText;
 
                 var parse = new DOMParser();
 
-                var html_new = parse.parseFromString(response,'text/html');
+                var html_new = parse.parseFromString(response, 'text/html');
 
-                var count_appearence=0;
+                var count_appearence = 0;
                 //Find all text nodes, and add their parents to a list
-                recursiveFindTextNodes(0,html_new.body,function (parent, node) {
-                    if(node.data.match(re)){
-                       count_appearence++;
+                recursiveFindTextNodes(0, html_new.body, function (parent, node) {
+                    if (node.data.match(re)) {
+                        count_appearence++;
                     }
                 });
 
-
-                //Displays the website and appearance number together async
-                console.log("The appearance in new website is:",this.responseURL.toString(),count_appearence.toString());
-
                 chrome.runtime.sendMessage({
-                    url:this.responseURL.toString(),
-                    num:count_appearence.toString(),
-                    limit:max
-                },function (response) {
-                    console.log(response);
-                });
-
-            }
-
-            if(this.status==403){
-                // alert('FORBIDDEN');
-                chrome.runtime.sendMessage({
-                    url:this.responseURL.toString(),
-                    num:"0",
-                    limit:max
-                },function (response) {
+                    url: this.responseURL.toString(),
+                    urlTitle: linkText,
+                    num: count_appearence.toString(),
+                    limit: hyperlinkMap.size
+                }, function (response) {
                     console.log(response);
                 });
             }
 
-            if(this.status==404){
-                // alert('PAGE NOT FOUND');
+            if (this.status == 403 || this.status == 404) {
                 chrome.runtime.sendMessage({
-                    url:this.responseURL.toString(),
-                    num:"0",
-                    limit:max
-                },function (response) {
+                    url: this.responseURL.toString(),
+                    urlTitle: linkText,
+                    num: "0",
+                    limit: hyperlinkMap.size
+                }, function (response) {
                     console.log(response);
                 });
-
             }
-
-
         }
+        xhr.ontimeout = () => {
+            hyperlinkMap.delete(link);
+        };
         xhr.send();
     }
 }
 
 function recursiveFindTextNodes(parent, node, func) {
     // nodeType 3 designates text
-    if(parent && node.nodeType === 3 ){
-        if(parent.tagName!=="SCRIPT" && parent.tagName!=="STYLE" && parent.tagName!=="IFRAME" && parent.tagName !=="CANVAS"
-        && parent.tagName!=="META"){
+    if (parent && node.nodeType === 3) {
+        if (parent.tagName !== "SCRIPT" && parent.tagName !== "STYLE" && parent.tagName !== "IFRAME" && parent.tagName !== "CANVAS"
+            && parent.tagName !== "META") {
             func(parent, node);
         }
-    }else{
+    } else {
         // Loop through children and make recursive calls
-        for(let i=0; i<node.childNodes.length; i++){
-            recursiveFindTextNodes(node, node.childNodes[i],func);
+        for (let i = 0; i < node.childNodes.length; i++) {
+            recursiveFindTextNodes(node, node.childNodes[i], func);
         }
     }
 
--- src/popup.js
+++ src/popup.js
@@ -1,34 +1,39 @@
-document.addEventListener('DOMContentLoaded',function () {
-    var btn = document.getElementById("press");
-    btn.addEventListener("click",clicked);
-
+document.addEventListener('DOMContentLoaded', function () {
+    var searchInput = document.getElementById("searchInput");
+    searchInput.addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+            onClick();
+        }
+    });
+    var btn = document.getElementById("findBtn");
+    btn.addEventListener("click", onClick);
 });
 
-function clicked() {
+function onClick() {
     createTable();
-    var val=document.getElementById("findInput").value;
+    var val = document.getElementById("searchInput").value;
 
 
-    chrome.tabs.query({currentWindow:true, active:true},
+    chrome.tabs.query({currentWindow: true, active: true},
         function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id,val,setCount)
+            chrome.tabs.sendMessage(tabs[0].id, val, setCount)
         })
 }
 
 function createTable() {
     //Table Headers
-    var tableHeader = ['','Hyperlinks','Appearances'];
-    var empTable=document.createElement('table');
-    empTable.setAttribute('id','empTable');
-    empTable.id='empTable';
+    var tableHeader = ['', 'Hyperlinks', 'Appearances'];
+    var empTable = document.createElement('table');
+    empTable.setAttribute('id', 'empTable');
+    empTable.id = 'empTable';
 
     var tr = empTable.insertRow(-1);
-    for(var h=0;h<tableHeader.length;h++){
+    for (var h = 0; h < tableHeader.length; h++) {
         var th = document.createElement('th');
-        th.innerHTML= tableHeader[h];
+        th.innerHTML = tableHeader[h];
         tr.appendChild(th);
     }
-    var div= document.getElementById('displayTable');
+    var div = document.getElementById('displayTable');
     div.appendChild(empTable);
 }
 
@@ -41,7 +46,8 @@ function setCount(res) {
     // document.body.appendChild(div)
 
 }
-var totalLinks=0;
+
+var totalLinks = 0;
 
 
 // global variables
@@ -50,212 +56,208 @@ var items = new Array();      // array of 'child' nodes
 var col = 2;                  // column for sorting
 var N = 0;
 
-chrome.runtime.onMessage.addListener(function (message,sender,sendResponse) {
-        // totalLinks+=1;
-        if(totalLinks!==message.limit-1){
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 
-            var textlink = message.url;
-            var value = message.num;
+    if (totalLinks !== message.limit - 1) {
+        var url = message.url;
+        var urlTitle = message.urlTitle;
+        if(!urlTitle){
+            urlTitle = url;
+        }
+        var value = message.num;
 
-            var empTable = document.getElementById('empTable');
-            var rowCount= empTable.rows.length;
-            var tr =empTable.insertRow(rowCount);
+        var empTable = document.getElementById('empTable');
+        var rowCount = empTable.rows.length;
+        var tr = empTable.insertRow(rowCount);
 
-            var td = document.createElement('td');
-
-
-            td=tr.insertCell(0);
-            //Create checkboxes for each hyperlinks
-            var check = document.createElement("INPUT");
-            check.setAttribute("type", "checkbox");
-            check.id = textlink;
-            td.appendChild(check);
+        var td = document.createElement('td');
 
 
-            td=tr.insertCell(1);
-            //Create label for each checkboxes for each hyperlinks
-            var label = document.createElement("a");
-            label.setAttribute('href',textlink);
-            label.setAttribute('style',"color:#0000FF;")
-            label.appendChild(document.createTextNode(textlink));
-            td.appendChild(label);
+        td = tr.insertCell(0);
+        //Create checkboxes for each hyperlinks
+        var check = document.createElement("INPUT");
+        check.setAttribute("type", "checkbox");
+        check.id = url;
+        td.appendChild(check);
 
 
-            td=tr.insertCell(2);
-            var num = document.createElement('num');
-            num.innerText = value;
-            num.id = value;
-            // num.setAttribute('type','number');
-            td.appendChild(num);
-
-            totalLinks+=1;
-        }else{
-            var textlink = message.url;
-            var value = message.num;
-
-            var empTable = document.getElementById('empTable');
-            var rowCount= empTable.rows.length;
-            var tr =empTable.insertRow(rowCount);
-
-            var td = document.createElement('td');
+        td = tr.insertCell(1);
+        //Create label for each checkboxes for each hyperlinks
+        var label = document.createElement("a");
+        label.setAttribute('href', url);
+        label.setAttribute('style', "color:#0000FF;")
+        label.appendChild(document.createTextNode(urlTitle));
+        td.appendChild(label);
 
 
+        td = tr.insertCell(2);
+        var num = document.createElement('num');
+        num.innerText = value;
+        num.id = value;
+        // num.setAttribute('type','number');
+        td.appendChild(num);
 
-            td=tr.insertCell(0);
-            //Create checkboxes for each hyperlinks
-            var check = document.createElement("INPUT");
-            check.setAttribute("type", "checkbox");
-            check.id = textlink;
-            td.appendChild(check);
+        totalLinks += 1;
+    } else {
+        var url = message.url;
+        var urlTitle = message.urlTitle;
+        if(!urlTitle){
+            urlTitle = url;
+        }
+        var value = message.num;
 
-            td=tr.insertCell(1);
-            //Create label for each checkboxes for each hyperlinks
-            var label = document.createElement("a");
-            label.setAttribute('href',textlink);
-            label.setAttribute('style',"color:#0000FF;")
-            label.appendChild(document.createTextNode(textlink));
-            td.appendChild(label);
+        var empTable = document.getElementById('empTable');
+        var rowCount = empTable.rows.length;
+        var tr = empTable.insertRow(rowCount);
 
-
-            td=tr.insertCell(2);
-            var num = document.createElement('num');
-            num.innerText = value;
-            // num.id = value;
-            // num.setAttribute('type','number');
-            td.appendChild(num);
+        var td = document.createElement('td');
 
 
-            totalLinks+=1;
-            // alert("SORT")
-            var sort = document.createElement("BUTTON");
-            sort.id="sort";
-            sort.innerText="Sort";
-            sort.setAttribute('style','float:left;');
-            document.body.appendChild(sort);
-            sort.onclick= function sorting() {
-                var table, rows, switching, i, x, y, shouldSwitch;
-                table = document.getElementById('empTable');
-                switching=true;
+        td = tr.insertCell(0);
+        //Create checkboxes for each hyperlinks
+        var check = document.createElement("INPUT");
+        check.setAttribute("type", "checkbox");
+        check.id = url;
+        td.appendChild(check);
 
-                while(switching){
-                    switching= false;
-                    rows=table.rows;
-                    // alert(rows.length-1);
-                    for(i=1;i<(rows.length-1);i++){
-                        shouldSwitch=false;
+        td = tr.insertCell(1);
+        //Create label for each checkboxes for each hyperlinks
+        var label = document.createElement("a");
+        label.setAttribute('href', url);
+        label.setAttribute('style', "color:#0000FF;")
+        label.appendChild(document.createTextNode(urlTitle));
+        td.appendChild(label);
 
 
-                        x = rows[i].getElementsByTagName("TD")[2];
-                        y = rows[i+1].getElementsByTagName("TD")[2];
-                        // alert(Number(x.innerText));
-                        // alert(x.id);
-                        if(Number(x.innerText)<Number(y.innerText)){
-                            // alert("IF");
-                            shouldSwitch=true;
-                            break;
-                        }
-                    }
-                    if(shouldSwitch){
+        td = tr.insertCell(2);
+        var num = document.createElement('num');
+        num.innerText = value;
+        // num.id = value;
+        // num.setAttribute('type','number');
+        td.appendChild(num);
 
-                        rows[i].parentNode.insertBefore(rows[i+1],rows[i]);
-                        switching=true;
+
+        totalLinks += 1;
+        // alert("SORT")
+        var sort = document.createElement("BUTTON");
+        sort.id = "sort";
+        sort.innerText = "Sort";
+        sort.setAttribute('style', 'float:left;');
+        document.body.appendChild(sort);
+        sort.onclick = function sorting() {
+            var table, rows, switching, i, x, y, shouldSwitch;
+            table = document.getElementById('empTable');
+            switching = true;
+
+            while (switching) {
+                switching = false;
+                rows = table.rows;
+                // alert(rows.length-1);
+                for (i = 1; i < (rows.length - 1); i++) {
+                    shouldSwitch = false;
+
+
+                    x = rows[i].getElementsByTagName("TD")[2];
+                    y = rows[i + 1].getElementsByTagName("TD")[2];
+                    // alert(Number(x.innerText));
+                    // alert(x.id);
+                    if (Number(x.innerText) < Number(y.innerText)) {
+                        // alert("IF");
+                        shouldSwitch = true;
+                        break;
                     }
                 }
+                if (shouldSwitch) {
 
-                // parent = document.getElementById('empTable');
-                // col = 2;
-                //
-                // items = parent.rows;
-                // N= items.length-1;
-                //
-                // quicksort(1,N,true);
-
-
-
-
-
+                    rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                    switching = true;
+                }
             }
 
-            //Creates button at the end of the list
-            var go = document.createElement("BUTTON");
-            go.id="go";
-            go.innerText = "Open";
-            go.setAttribute('style','float:right;')
-            document.body.appendChild(go);
-            //A function to save all the checked hyperlinks to an array
-            go.onclick = function respond () {
-                // var container=document.getElementById("container");
+        }
 
-                var grid = document.getElementById('empTable');
-                var checklist = grid.getElementsByTagName("INPUT");
-                // var checklist=container.children;
+        //Creates button at the end of the list
+        var go = document.createElement("BUTTON");
+        go.id = "go";
+        go.innerText = "Open";
+        go.setAttribute('style', 'float:right;')
+        document.body.appendChild(go);
+        //A function to save all the checked hyperlinks to an array
+        go.onclick = function respond() {
+            // var container=document.getElementById("container");
 
-                // var total = totalLinks*3;
-                var total = totalLinks;
+            var grid = document.getElementById('empTable');
+            var checklist = grid.getElementsByTagName("INPUT");
+            // var checklist=container.children;
 
-                var checkboxesChecked=[];         //Saves all the checked hyperlinks
-                var num_clicked_links=0;
-                //If the checkbox is checked, then add it to the array
-                for(var j=0;j<total;j++){
-                    if(checklist[j].checked==true){
-                        checkboxesChecked.push(checklist[j].id);
-                        num_clicked_links=num_clicked_links+1;
+            // var total = totalLinks*3;
+            var total = totalLinks;
 
-                    }
-                }
-                //Opens the checked hyperlinks
-                var windowCreated=0;
-                var windowCreatedID;
-                for(var k=0; k<num_clicked_links;k++){
-                    //Opens the checked hyperlinks in new tabs in same window
-                    chrome.tabs.create({url:checkboxesChecked[k]});
+            var checkboxesChecked = [];         //Saves all the checked hyperlinks
+            var num_clicked_links = 0;
+            //If the checkbox is checked, then add it to the array
+            for (var j = 0; j < total; j++) {
+                if (checklist[j].checked == true) {
+                    checkboxesChecked.push(checklist[j].id);
+                    num_clicked_links = num_clicked_links + 1;
 
                 }
+            }
+            //Opens the checked hyperlinks
+            var windowCreated = 0;
+            var windowCreatedID;
+            for (var k = 0; k < num_clicked_links; k++) {
+                //Opens the checked hyperlinks in new tabs in same window
+                chrome.tabs.create({url: checkboxesChecked[k]});
+
             }
         }
+    }
 
 })
 
-function quicksort(m,n,desc) {
+function quicksort(m, n, desc) {
     alert('INSIDE QUICKSORT');
-    if(n <= m+1) return;
+    if (n <= m + 1) return;
 
 
-    if((n-m) == 2){
-        if(compare(get(n-1), get(m),desc)){ exchange(i,m);}
+    if ((n - m) == 2) {
+        if (compare(get(n - 1), get(m), desc)) {
+            exchange(i, m);
+        }
         return;
     }
 
-    i = m+1;
+    i = m + 1;
     j = n - 1;
     alert('BEFORE FIRST IF');
-    if(compare(get(m), get(i), desc)) {
+    if (compare(get(m), get(i), desc)) {
         alert('INSIDE FIRST IF');
         exchange(i, m);
         alert('AFTER EXCHANGE');
     }
-    // console.log('INSIDE POPUP');
+
     alert('AFTER FIRST IF');
-    if(compare(get(j), get(m), desc)) {
+    if (compare(get(j), get(m), desc)) {
         exchange(m, j);
     }
-    if(compare(get(m), get(i), desc)) {
+    if (compare(get(m), get(i), desc)) {
         exchange(i, m);
     }
 
     alert('BEFORE PIVOT');
     pivot = get(m);
 
-    while(true) {
+    while (true) {
         j--;
-        while(compare(pivot, get(j), desc)){
+        while (compare(pivot, get(j), desc)) {
             j--;
         }
         i++;
-        while(compare(get(i), pivot, desc)){
+        while (compare(get(i), pivot, desc)) {
             i++;
         }
-        if(j <= i) {
+        if (j <= i) {
             break;
         }
         exchange(i, j);
@@ -263,30 +265,27 @@ function quicksort(m,n,desc) {
 
     exchange(m, j);
 
-    if((j-m) < (n-j)) {
+    if ((j - m) < (n - j)) {
         quicksort(m, j, desc);
-        quicksort(j+1, n, desc);
+        quicksort(j + 1, n, desc);
     } else {
-        quicksort(j+1, n, desc);
+        quicksort(j + 1, n, desc);
         quicksort(m, j, desc);
     }
 }
 
-function compare(val1, val2, desc)
-{
+function compare(val1, val2, desc) {
     alert('INSIDE COMPARE');
     return (desc) ? val1 > val2 : val1 < val2;
 }
 
-function exchange(i, j)
-{
+function exchange(i, j) {
     alert('INSIDE EXCHANGE');
     // exchange adjacent items
     parent.insertBefore(items[i], items[j]);
 }
 
-function get(i)
-{
+function get(i) {
     alert('INSIDE GET');
     // var node = items[i].getElementsByTagName("TD")[col];
     // if(node.childNodes.length == 0) return "";
